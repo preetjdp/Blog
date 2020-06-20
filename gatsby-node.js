ae2bb2d3bb40f05da86a5edc2161903b755cc333
Preet Parekh <preetjdp123@gmail.com>
2020-06-20 23:29:05 +0530

    Generate Pages for blogs

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,21 +4,22 @@ const { createFilePath } = require(`gatsby-source-filesystem`)
 exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions
 
-  const blogPost = path.resolve(`./src/templates/blog-post.js`)
+  const blogPost = path.resolve(`./src/templates/blog-post.tsx`)
   const result = await graphql(
     `
       {
-        allMarkdownRemark(
-          sort: { fields: [frontmatter___date], order: DESC }
-          limit: 1000
-        ) {
-          edges {
-            node {
-              fields {
-                slug
-              }
-              frontmatter {
-                title
+        github {
+          viewer {
+            repository(name: "Blog") {
+              issues(first: 20) {
+                nodes {
+                  id
+                  number
+                  title
+                  resourcePath
+                  createdAt  
+                  bodyHTML
+                }
               }
             }
           }
@@ -32,17 +33,17 @@ exports.createPages = async ({ graphql, actions }) => {
   }
 
   // Create blog posts pages.
-  const posts = result.data.allMarkdownRemark.edges
+  // const posts = result.data.allMarkdownRemark.edges
+  const posts = result.data.github.viewer.repository.issues.nodes
 
   posts.forEach((post, index) => {
     const previous = index === posts.length - 1 ? null : posts[index + 1].node
     const next = index === 0 ? null : posts[index - 1].node
-
     createPage({
-      path: post.node.fields.slug,
+      path: post.number.toString(),
       component: blogPost,
       context: {
-        slug: post.node.fields.slug,
+        slug: post.number,
         previous,
         next,
       },
