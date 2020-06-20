c35d5b03414aa57218a5dcad65a3e51197b368b9
Preet Parekh <preetjdp123@gmail.com>
2020-06-20 18:12:18 +0530

    New Layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,70 +1,147 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from 'react';
+import { Link } from 'gatsby';
+import Toggle from './Toggle';
+import Helmet from 'react-helmet';
 
-import { rhythm, scale } from "../utils/typography"
+import { rhythm, scale } from '../utils/typography';
+import sun from '../assets/sun.png';
+import moon from '../assets/moon.png';
 
-const Layout = ({ location, title, children }) => {
-  const rootPath = `${__PATH_PREFIX__}/`
-  let header
-
-  if (location.pathname === rootPath) {
-    header = (
-      <h1
-        style={{
-          ...scale(1.5),
-          marginBottom: rhythm(1.5),
-          marginTop: 0,
-        }}
-      >
-        <Link
-          style={{
-            boxShadow: `none`,
-            color: `inherit`,
-          }}
-          to={`/`}
-        >
-          {title}
-        </Link>
-      </h1>
-    )
-  } else {
-    header = (
-      <h3
-        style={{
-          fontFamily: `Montserrat, sans-serif`,
-          marginTop: 0,
-        }}
-      >
-        <Link
-          style={{
-            boxShadow: `none`,
-            color: `inherit`,
-          }}
-          to={`/`}
-        >
-          {title}
-        </Link>
-      </h3>
-    )
+class Layout extends React.Component {
+  state = {
+    theme: null,
+  };
+  componentDidMount() {
+    this.setState({ theme: window.__theme });
+    window.__onThemeChange = () => {
+      this.setState({ theme: window.__theme });
+    };
   }
-  return (
-    <div
-      style={{
-        marginLeft: `auto`,
-        marginRight: `auto`,
-        maxWidth: rhythm(24),
-        padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
-      }}
-    >
-      <header>{header}</header>
-      <main>{children}</main>
-      <footer>
-        © {new Date().getFullYear()}, Built with
-        {` `}
-        <a href="https://www.gatsbyjs.org">Gatsby</a>
-      </footer>
-    </div>
-  )
+  renderHeader() {
+    const { location, title } = this.props;
+    const rootPath = `${__PATH_PREFIX__}/`;
+
+    if (location.pathname === rootPath) {
+      return (
+        <h1
+          style={{
+            ...scale(0.75),
+            marginBottom: 0,
+            marginTop: 0,
+          }}
+        >
+          <Link
+            style={{
+              boxShadow: 'none',
+              textDecoration: 'none',
+              color: 'var(--textTitle)',
+            }}
+            to={'/'}
+          >
+            {title}
+          </Link>
+        </h1>
+      );
+    } else {
+      return (
+        <h3
+          style={{
+            fontFamily: 'Montserrat, sans-serif',
+            marginTop: 0,
+            marginBottom: 0,
+            height: 42, // because
+            lineHeight: '2.625rem',
+          }}
+        >
+          <Link
+            style={{
+              boxShadow: 'none',
+              textDecoration: 'none',
+              color: 'rgb(255, 167, 196)',
+            }}
+            to={'/'}
+          >
+            {title}
+          </Link>
+        </h3>
+      );
+    }
+  }
+  render() {
+    const { children } = this.props;
+
+    return (
+      <div
+        style={{
+          color: 'var(--textNormal)',
+          background: 'var(--bg)',
+          transition: 'color 0.2s ease-out, background 0.2s ease-out',
+          minHeight: '100vh',
+        }}
+      >
+        <Helmet
+          meta={[
+            {
+              name: 'theme-color',
+              content: this.state.theme === 'light' ? '#ffa8c5' : '#282c35',
+            },
+          ]}
+        />
+        <div
+          style={{
+            marginLeft: 'auto',
+            marginRight: 'auto',
+            maxWidth: rhythm(24),
+            padding: `2.625rem ${rhythm(3 / 4)}`,
+          }}
+        >
+          <header
+            style={{
+              display: 'flex',
+              justifyContent: 'space-between',
+              alignItems: 'center',
+              marginBottom: '2.625rem',
+            }}
+          >
+            {this.renderHeader()}
+            {this.state.theme !== null ? (
+              <Toggle
+                icons={{
+                  checked: (
+                    <img
+                      src={moon}
+                      width="16"
+                      height="16"
+                      role="presentation"
+                      style={{ pointerEvents: 'none' }}
+                    />
+                  ),
+                  unchecked: (
+                    <img
+                      src={sun}
+                      width="16"
+                      height="16"
+                      role="presentation"
+                      style={{ pointerEvents: 'none' }}
+                    />
+                  ),
+                }}
+                checked={this.state.theme === 'dark'}
+                onChange={e =>
+                  window.__setPreferredTheme(
+                    e.target.checked ? 'dark' : 'light'
+                  )
+                }
+              />
+            ) : (
+              <div style={{ height: '24px' }} />
+            )}
+          </header>
+          {children}
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Layout
+export default Layout;
