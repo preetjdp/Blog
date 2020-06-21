530dfbb5c17a7a643305d741e210c5aeac9c0a63
Preet Parekh <preetjdp123@gmail.com>
2020-06-21 13:52:39 +0530

    Added Lang to HTML attributes

--- src/components/seo.tsx
+++ src/components/seo.tsx
@@ -10,7 +10,7 @@ import PropTypes from "prop-types"
 import { Helmet } from "react-helmet"
 import { useStaticQuery, graphql } from "gatsby"
 
-const SEO = ({ description, lang, meta, title }) => {
+const SEO = ({ description, meta, title }) => {
   const { site, icon } = useStaticQuery(
     graphql`
       query {
@@ -42,10 +42,10 @@ const SEO = ({ description, lang, meta, title }) => {
 
   return (
     <Helmet
-      htmlAttributes={{
-        lang,
-      }}
       title={title}
+      htmlAttributes={{
+        lang: `en`
+      }}
       titleTemplate={`%s | ${site.siteMetadata.title}`}
       meta={[
         {
@@ -98,14 +98,12 @@ const SEO = ({ description, lang, meta, title }) => {
 }
 
 SEO.defaultProps = {
-  lang: `en`,
   meta: [],
   description: ``,
 }
 
 SEO.propTypes = {
   description: PropTypes.string,
-  lang: PropTypes.string,
   meta: PropTypes.arrayOf(PropTypes.object),
   title: PropTypes.string.isRequired,
 }
